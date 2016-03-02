d8d08465db6b14153ade473739fdfa3ad5a452a0
ronathan <ronaldvictorino@gmail.com>
2016-03-02 15:48:58 -0500

    Added animate.css and some basic animations

--- js/home.js
+++ js/home.js
@@ -8,8 +8,8 @@
 
 	var waypoints = $('#home-about-me').waypoint({
 		handler: function(direction) {
-			$('#about-me-picture').addClass("animated zoomIn");
-			$('#about-me-text').addClass("animated fadeInLeft");
+			$('#about-me-picture').removeClass("hidden").addClass("animated zoomIn");
+			$('#about-me-text').removeClass("hidden").addClass("animated fadeInLeft");
 		},
 		offset: "50%"
 	});
